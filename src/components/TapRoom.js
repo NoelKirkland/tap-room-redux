674e35d98f3358d2fea5c6654711935a0056b226
Noel Kirkland <noelkirkland@gmail.com>
2020-09-11 09:43:10 -0700

    create a class component called TapRoom

---
+++ src/components/TapRoom.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class TapRoom extends React.Component {
+
+  constructor(){
+    super(props);
+    this.state = {}
+  };
+
+  render(){
+
+    return(
+      <React.Fragment>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default TapRoom;
