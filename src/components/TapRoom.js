bd3344e19f40ab9a51881676f0df2050a38fa2c4
Noel Kirkland <noelkirkland@gmail.com>
2020-09-11 11:51:10 -0700

    add TapRoom render logic

--- src/components/TapRoom.js
+++ src/components/TapRoom.js
@@ -17,10 +17,23 @@ class TapRoom extends React.Component {
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
 
+    if (this.state.selectedTap != null){
+      currentlyVisibleState = <TapDetail tap = {this.state.selectedTap}/>;
+      buttonText = "Return to Tap List";
+    } else if (this.state.formVisibleOnPage){
+      currentlyVisibleState = <NewTapForm onNewTapCreation={this.handleAddingNewTapToList}/>;
+      buttonText = "Return to Tap List";
+    } else {
+      currentlyVisibleState = <TapList tapList={this.state.masterTapList} onTapSelection={this.handleChangingSelectedTap}/>;
+      cuttonText = "Add Tap to List";
+    }
     return(
       <React.Fragment>
-
+        {currentlyVisibleState}
+        <button onClick={this.handlClick}>{buttonText}</button>
       </React.Fragment>
     )
   }
