c837d1a78bf7dc7d20ace920be0f4e2283f000ed
Noel Kirkland <noelkirkland@gmail.com>
2020-09-18 14:12:37 -0700

    successfully pass third test

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,18 +1,22 @@
 export default (state = {}, action) => {
   const { name, brand, price, flavor, pints, id } = action;
   switch (action.type){
-    case 'ADD_TAP':
-      return Object.assign({}, state, {
-        [id]: {
-          name: name,
-          brand: brand,
-          price: price,
-          flavor: flavor,
-          pints: pints,
-          id: id
-        }
-      });
-      default:
-        return state;
+  case 'ADD_TAP':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        flavor: flavor,
+        pints: pints,
+        id: id
+      }
+    });
+  case 'DELETE_TAP':
+    const newState = { ...state };
+    delete newState[id];
+    return newState;
+  default:
+    return state;
   }
 };
