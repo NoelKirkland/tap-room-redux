ccd7ac1604de053973058d784e1b7f3981764a6b
Noel Kirkland <noelkirkland@gmail.com>
2020-09-18 14:10:17 -0700

    successfully fail third test

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -2,6 +2,29 @@ import tapListReducer from '../../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
 
+  const currentState = {
+    1: {
+      name: 'Ginger Cure',
+      brand: 'Brew Dr.',
+      price: '4.00',
+      flavor: 'ginger and lemon',
+      pints: 124,
+      id: 1
+    },
+    2: {
+      name: 'Very Berry',
+      brand: 'Brew Dr.',
+      price: '3.75',
+      flavor: 'rasberry and blackberry',
+      pints: 124,
+      id: 2
+    }
+  }
+
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(tapListReducer({}, { type: null })).toEqual({});
+  });
+
   let action;
   const tapData = {
     name: 'Ginger Cure',
@@ -35,7 +58,20 @@ describe('tapListReducer', () => {
     });
   });
 
-  test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(tapListReducer({}, { type: null })).toEqual({});
-  });
+  test('Should successfully delete a tap', () => {
+    action = {
+      type: 'DELETE_TAP',
+      id: 1
+    };
+    expect(tapListReducer(currentState, action)).toEqual({
+      2: {
+        name: 'Very Berry',
+        brand: 'Brew Dr.',
+        price: '3.75',
+        flavor: 'rasberry and blackberry',
+        pints: 124,
+        id: 2
+      }
+    });
+  })
 });
